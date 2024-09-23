e684a8aff21e35de95f1741fbe7e67c94f9e1f05
nanasantos23 <163125549+nanasantos23@users.noreply.github.com>
2024-09-23 13:57:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
 const nuvem = document.querySelector(".nuvem");
 const startButton = document.querySelector(".start");
 const gameOverScreen = document.querySelector(".game-over");
+const scoreElement = document.querySelector(".score")
 
 audioStart = new Audio("./sound/audio_theme.mp3");
-const gameOverSound = new Audio("./sound/audio.gameover.mp3");
+const gameOverSound = new Audio("./sound/audio_gameover.mp3");
 
 let gameStarted = false;
+let score = 0;
 
 const startGame = () => {
-gameStarted = true;
-audioStart.play();
+  gameStarted = true;
+  audioStart.play();
 
-pipe.style.animation = "pipe-animation 1.5s infinite linear";
+  pipe.style.animation = "pipe-animation 1.5s infinite linear";
 
-startButton.style.display = "none";
-mario.style.opacity = "1";
-pipe.style.opacity = "1";
-clouds.style.opacity = "1";
+  startButton.style.display = "none";
+  mario.style.opacity = "1";
+  pipe.style.opacity = "1";
+  nuvem.style.opacity = "1";
 
 }
 
@@ -34,6 +35,13 @@ const jump = () => {
 }
 }
 
+const updateScore = () => {
+  score += 1;
+  scoreElement.textContent = score;
+  const animationSpeed = 1.5/(1 + score/ 500);
+  pipe.style.animation = pipe-animation ${animationSpeed}s infinite linear;
+}
+
 const loop = setInterval (() => {
 const pipePosition = pipe.offsetLeft;
 const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
@@ -45,21 +53,24 @@ if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
     mario.style.animation = "none";
     mario.style.left = `${marioPosition}px`;
 
-    mario.src = ". /img/game-over.png";
+    mario.src = "./img/game-over.png";
     mario.style.width = "75px";
     mario.style.marginLeft = "50px";
     audioStart.pause();
-  
+
     gameOverSound.play();
   
     clearInterval(loop);
     gameOverScreen.style.display = "flex";
+  } else if (pipePosition < 0 && gameStarted) {
+    updateScore();
+  pipe.style.left = '';
   }
 },10);
 
 document.addEventListener("keydown", jump);
 
 const restartGame = () => {
-
+  
   window.location.reload();
 }
